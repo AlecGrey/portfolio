2e37bfc83dab34fccc7d5155e71ae82fb0f9d227
AlecGrey <alecgrey.coding@gmail.com>
2021-02-25 17:22:37 -0800

    animated circle on about page scrollbar

--- scripts/animation/aboutPage.js
+++ scripts/animation/aboutPage.js
@@ -2,13 +2,33 @@ export function aboutLoad() {
     // GRAB PAGE ELEMENTS
     const aboutDiv = document.getElementById('about')
     const scrollBar = document.querySelector('.scroll-bar')
+    const circle = document.querySelector('.scroll-box > .circle')
     const scrollItems = document.querySelectorAll('.scroll-box > p')
     // ESTABLISH NEW TIMELINE
     const t1 = new TimelineMax()
     // reveal about page
     t1.fromTo(aboutDiv, 0.1, { display: 'none' }, { display: 'block' })
     // reveal scroll bar
-    t1.fromTo(scrollBar, 0.8, { height: '0%' }, { height: '110%' }, 0)
+    t1.fromTo(scrollBar, 0.8, { height: '0%' }, { height: '100%' }, 0)
+    // circle animation: light up
+    t1.fromTo(
+        circle, 1,
+        { alpha: 0 },
+        { alpha: 1, ease: Power4.easeOut }
+        // happens immediately after scrollbar finishes
+    )
+    t1.fromTo(
+        circle, 0.3,
+        { boxShadow: '0 0 0 0 white' },
+        { boxShadow: '0 0 2.5rem 0.8rem white', ease: Power4.easeOut },
+        '-=1'
+    )
+    t1.fromTo(
+        circle, 0.4,
+        { boxShadow: '0 0 2.5rem 0.8rem white' },
+        { boxShadow: '0 0 2rem 0.5rem white', ease: Power3.easeOut },
+        '-=0.7'
+    )
     // reveal scroll items
     let length = scrollItems.length
     for (let i = 0; i < length; i++) {
@@ -33,7 +53,7 @@ export function aboutHide() {
     // ESTABLISH NEW TIMELINE
     const t1 = new TimelineMax()
     // scroll down the scroll bar
-    t1.fromTo(scrollBar, 1, { height: '110%' }, { height: '0%', ease: Power3.easeOut }, 0)
+    t1.fromTo(scrollBar, 1, { height: '100%' }, { height: '0%', ease: Power3.easeOut }, 0)
     // hide each scroll item
     let j = 0
     for (let item of scrollItems) {
