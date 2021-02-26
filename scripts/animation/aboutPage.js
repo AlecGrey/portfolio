234dcef39ec42d19880cef965fb426e5e0873f2f
AlecGrey <alecgrey.coding@gmail.com>
2021-02-26 11:22:44 -0800

    added scrollbar animation to about page.  improved navlink transition between pages

--- scripts/animation/aboutPage.js
+++ scripts/animation/aboutPage.js
@@ -2,7 +2,7 @@ export function aboutLoad() {
     // GRAB PAGE ELEMENTS
     const aboutDiv = document.getElementById('about')
     const scrollBar = document.querySelector('.scroll-bar')
-    const circle = document.querySelector('.scroll-box > .circle')
+    const innerScrollBar = document.querySelector('.scroll-bar.inner')
     const scrollItems = document.querySelectorAll('.scroll-box > p')
     // ESTABLISH NEW TIMELINE
     const t1 = new TimelineMax()
@@ -10,24 +10,12 @@ export function aboutLoad() {
     t1.fromTo(aboutDiv, 0.1, { display: 'none' }, { display: 'block' })
     // reveal scroll bar
     t1.fromTo(scrollBar, 0.8, { height: '0%' }, { height: '100%' }, 0)
-    // circle animation: light up
+    // expand inner scrollbar
     t1.fromTo(
-        circle, 1,
-        { alpha: 0 },
-        { alpha: 1, ease: Power4.easeOut }
-        // happens immediately after scrollbar finishes
-    )
-    t1.fromTo(
-        circle, 0.3,
-        { boxShadow: '0 0 0 0 white' },
-        { boxShadow: '0 0 2.5rem 0.8rem white', ease: Power4.easeOut },
-        '-=1'
-    )
-    t1.fromTo(
-        circle, 0.4,
-        { boxShadow: '0 0 2.5rem 0.8rem white' },
-        { boxShadow: '0 0 2rem 0.5rem white', ease: Power3.easeOut },
-        '-=0.7'
+        innerScrollBar, 1,
+        { height: '0%', bottom: '87.5%', alpha: 0 },
+        { height: '25%', bottom: '75%', alpha: 1, ease: Power2.easeOut },
+        0.8
     )
     // reveal scroll items
     let length = scrollItems.length
@@ -49,9 +37,12 @@ export function aboutHide() {
     // GRAB PAGE ELEMENTS
     const aboutDiv = document.getElementById('about')
     const scrollBar = document.querySelector('.scroll-bar')
+    const innerScrollBar = document.querySelector('.scroll-bar.inner')
     const scrollItems = document.querySelectorAll('.scroll-box > p')
     // ESTABLISH NEW TIMELINE
     const t1 = new TimelineMax()
+    // scroll & hide inner bar
+    t1.fromTo(innerScrollBar, 0.1, { height: '25%' }, { bottom: '0%', height: '0%', ease: Power3.easeOut }, 0)
     // scroll down the scroll bar
     t1.fromTo(scrollBar, 1, { height: '100%' }, { height: '0%', ease: Power3.easeOut }, 0)
     // hide each scroll item
@@ -70,4 +61,15 @@ export function aboutHide() {
 
     console.log(t1.totalDuration())
     return t1
+}
+
+export function scrollToItemByIndex(i) {
+    // grab inner scroll bar
+    const innerScrollBar = document.querySelector('.scroll-bar.inner')
+    // find desired position
+    const position = `${75 - (25 * i)}%`
+    // set timeline
+    const t1 = new TimelineMax()
+    // move scrollbar to desired position
+    t1.to(innerScrollBar, 1, { bottom: position, ease: Power4.easeOut })
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,28 @@
 import { welcomeLoad, welcomeHide } from './animation/welcomePage.js'
-import { aboutLoad, aboutHide } from './animation/aboutPage.js'
+import { aboutLoad, aboutHide, scrollToItemByIndex } from './animation/aboutPage.js'
 import navbarLoad from './animation/navbarLoad.js';
 
 // ================================================================
 //                    GRAB NECESSARY PAGE TAGS
 // ================================================================
 
+// nav tags
 const navWelcome = document.querySelector('a.welcome-link')
 const navAbout = document.querySelector('a.about-link')
 const navProjects = document.querySelector('a.projects-link')
 const navContact = document.querySelector('a.contact-link')
+// welcome tags
 const welcomeToAboutTag = document.querySelector('h3.welcome-h3 > span')
+// about tags
+const atAGlance = document.querySelector('p.glance')
+const education = document.querySelector('p.education')
+const skills = document.querySelector('p.skills')
+const otherSide = document.querySelector('p.other-side')
+
 // array of all links
 const navLinks = [navWelcome, navAbout, navProjects, navContact]
+// array of all about page scroll items
+const aboutScrollItems = [atAGlance, education, skills, otherSide]
 // ALL PAGE ANIMATIONS
 const animations = {}
 
@@ -55,13 +65,7 @@ const hideCurrentSection = () => {
     }
 }
 
-const revealSectionByTag = tag => {
-    // if it is already selected, no action required
-    if (tag.classList.contains('selected')) return
-    // grab current tag's class
-    const name = tag.className
-    // add selected to classList
-    tag.classList.add('selected')
+const revealSectionByTagName = name => {
     // execute animation depending on className
     switch(name) {
         case 'welcome-link':
@@ -89,8 +93,14 @@ const revealSectionByTag = tag => {
 const switchPageOnNavClick = e => {
     // DO NOTHING IF WE CLICK THE CURRENTLY SELECTED LINK!
     if (e.target.classList.contains('selected')) return
+    // begin animation to hide current page
     hideCurrentSection()
-    setTimeout(() => revealSectionByTag(e.target), 1000)
+    // grab className of selected page
+    const name = e.target.className
+    // add selected to classList
+    e.target.classList.add('selected')
+    // after hide animation ends, reveal new page by name
+    setTimeout(() => revealSectionByTagName(name), 1000)
 }
 
 const playAnimation = animationName => {
@@ -101,10 +111,35 @@ const playAnimation = animationName => {
     }
 }
 
+const setNavLinkEvents = () => {
+    for (const link of navLinks) {
+        link.addEventListener('click', switchPageOnNavClick)
+    }
+}
+
 // WELCOME PAGE FUNCTIONS
 const switchToAboutPage = () => {
     hideCurrentSection()
     setTimeout(() => revealSectionByTag(navAbout), 1000)
+}
+
+// ABOUT PAGE FUNCTIONS
+const deselectCurrentItem = () => {
+    document.querySelector('.scroll-box > p.selected').classList.remove('selected')
+}
+
+const setScrollItemEvents = () => {
+    let n = 0
+    for (const item of aboutScrollItems) {
+        let i = n
+        item.addEventListener('click', e => {
+            if (e.target.classList.contains('selected')) return
+            deselectCurrentItem()
+            e.target.classList.add('selected')
+            scrollToItemByIndex(i)
+        })
+        n += 1
+    }
 }
 
 // ================================================================
@@ -112,11 +147,10 @@ const switchToAboutPage = () => {
 // ================================================================
 
 // NAV EVENTS
-for (const link of navLinks) {
-    link.addEventListener('click', switchPageOnNavClick)
-}
+setNavLinkEvents()
 // WELCOME PAGE EVENTS
 welcomeToAboutTag.addEventListener('click', switchToAboutPage)
-
+// ABOUT PAGE EVENTS
+setScrollItemEvents()
 // LOAD IN THE PAGE
 pageLoad()
