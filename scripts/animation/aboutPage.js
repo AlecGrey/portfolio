79e770d18930ff285891f30ea8422f663b9af146
AlecGrey <alecgrey.coding@gmail.com>
2021-02-25 16:19:57 -0800

    updated about section nav icons

--- scripts/animation/aboutPage.js
+++ scripts/animation/aboutPage.js
@@ -8,15 +8,15 @@ export function aboutLoad() {
     // reveal about page
     t1.fromTo(aboutDiv, 0.1, { display: 'none' }, { display: 'block' })
     // reveal scroll bar
-    t1.fromTo(scrollBar, 1, { height: '0%' }, { height: '100%' }, 0)
+    t1.fromTo(scrollBar, 0.8, { height: '0%' }, { height: '110%' }, 0)
     // reveal scroll items
     let length = scrollItems.length
     for (let i = 0; i < length; i++) {
-        let delay = (1 / length) * i
+        let delay = (0.8 / length) * i
         t1.fromTo(
-            scrollItems[length - i - 1], 0.5,
+            scrollItems[length - i - 1], 1.5,
             { alpha: 0, x: '-10px' },
-            { alpha: 1, x: 0, ease: Power3.easeOut },
+            { alpha: 1, x: 0, ease: Power2.easeOut },
             delay
         )
     }
@@ -33,7 +33,7 @@ export function aboutHide() {
     // ESTABLISH NEW TIMELINE
     const t1 = new TimelineMax()
     // scroll down the scroll bar
-    t1.fromTo(scrollBar, 1, { height: '100%' }, { height: '0%', ease: Power3.easeOut }, 0)
+    t1.fromTo(scrollBar, 1, { height: '110%' }, { height: '0%', ease: Power3.easeOut }, 0)
     // hide each scroll item
     let j = 0
     for (let item of scrollItems) {
